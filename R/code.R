9e06eb3492273efb1977ee62e224575105706cc5
Toby Dylan Hocking <tdhock5@gmail.com>
2014-04-04 22:13:52 -0400

    better error msg

--- R/code.R
+++ R/code.R
@@ -12,8 +12,12 @@ requireGitHub_code <- function(...){
   for(pkg.i in seq_along(pkgs)){
     pkg.name <- as.character(pkgs[[pkg.i]])
     pkg.info <- packageDescription(pkg.name)
-    repo.code[[pkg.i]] <- with(pkg.info, {
-      sprintf("%s/%s@%s", GithubUsername, GithubRepo, GithubSHA1)
+    tryCatch({
+      repo.code[[pkg.i]] <- with(pkg.info, {
+        sprintf("%s/%s@%s", GithubUsername, GithubRepo, GithubSHA1)
+      })
+    }, error=function(e){
+      stop("GitHub meta-data not in ", pkg.name, " DESCRIPTION")
     })
   }
   txt <- deparse(repo.code)
